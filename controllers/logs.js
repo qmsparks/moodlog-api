6e815537a7000872fd4b4d4a3277f88afb6de31e
qsparks <q.m.sparks@gmail.com>
2021-04-06 15:06:20 -0500

    log deletion works

--- controllers/logs.js
+++ controllers/logs.js
@@ -78,9 +78,20 @@ const update = async (req, res) => {
 
 const destroy = async (req, res) => {
   try {
-    
+    // TODO surprise surprise, gotta adjust this for auth later
+
+    const deletedLog = await db.Log.findByIdAndDelete(req.params.logId);
+    // TODO include a check for if the log exists first, like in the update function
+    return res.status(200).json({
+      message: "Log deleted",
+      log: deletedLog
+    })
   } catch (err) {
-    
+    console.log(err);
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again."
+    })
   }
 }
 
