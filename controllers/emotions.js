14165228df0fa20f05b11257943c5e2ead8c84f2
qsparks <q.m.sparks@gmail.com>
2021-04-06 15:23:17 -0500

    emotion update function works

--- controllers/emotions.js
+++ controllers/emotions.js
@@ -19,9 +19,21 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   try {
-    
+    const updatedEmotion = await db.Emotion.findByIdAndUpdate(req.params.emotionId, req.body, {new: true});
+
+    if (!updatedEmotion) return res.status(200).json({
+      message: "No emotion with that id found in database"
+    })
+
+    return res.status(200).json({
+      emotion: updatedEmotion
+    })
   } catch (err) {
-    
+    console.log(err);
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again."
+    })
   }
 }
 
@@ -29,7 +41,11 @@ const destroy = async (req, res) => {
   try {
     
   } catch (err) {
-    
+    console.log(err);
+    return res.status(500).json({
+      status: 500,
+      message: "Something went wrong. Please try again."
+    })
   }
 }
 
