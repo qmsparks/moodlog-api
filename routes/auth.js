c9f172ba7d26d3ca96764234d94a86c4f0de16df
qsparks <q.m.sparks@gmail.com>
2021-04-09 17:43:52 -0500

    sets up cors

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const { auth } = require('../controllers');
 const authRequired = require('../middleware/authRequired');
 
-
-router.get('/', authRequired, auth.verify)
 router.post('/register', auth.register);
 router.post('/login', auth.login);
 
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // ANCHOR external imports
 const express = require('express');
+const cors = require('cors');
 
 // ANCHOR internal imports
 const { logs, emotions, thoughts, auth } = require('./routes');
+const authRequired = require('./middleware/authRequired');
 
 // ANCHOR config
 const app = express();
@@ -11,16 +13,19 @@ const PORT = process.env.PORT;
 
 // ANCHOR middleware
 app.use(express.json());
+app.use(cors());
 
 // ANCHOR routes
 app.get('/', (req, res) => {
   res.send('<h1>Do your dang CBT homework</h1>')
 })
+app.use('/api/v1/auth', auth);
+
+app.use(authRequired);
 
 app.use('/api/v1/logs', logs);
 app.use('/api/v1/emotions', emotions);
 app.use('/api/v1/thoughts', thoughts);
-app.use('/api/v1/auth', auth);
 
 
 // ANCHOR connection
